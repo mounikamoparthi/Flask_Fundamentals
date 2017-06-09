1193d3c8bf9927eb5260a37842110390241623ad
Mounika <mounika.tunga9@gmail.com>
2017-06-09 13:03:27 -0700

    counter views using session in a function

--- Counter/counter.py
+++ Counter/counter.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request, session
 app = Flask(__name__)
+app.secret_key = 'hebflbbdvdbf'
+def sumSession():
+    try:
+        session['counter'] += 1
+    except KeyError:
+        session['counter'] = 1
+    return session['counter']
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", counter =sumSession())
+app.run(debug=True) 
