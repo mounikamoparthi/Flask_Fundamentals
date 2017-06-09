f9adf172d1621a15e2a9ccfb485e44cd913516f8
Mounika <mounika.tunga9@gmail.com>
2017-06-09 15:41:37 -0700

    added counter2 button

--- Counter/counter.py
+++ Counter/counter.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, request, session
 app = Flask(__name__)
 app.secret_key = 'hebflbbdvdbf'
-def sumSession():
+def sumSession(i):
     try:
-        session['counter'] += 1
+        session['counter'] += i 
     except KeyError:
         session['counter'] = 1
     return session['counter']
 @app.route('/')
 def index():
-    return render_template("index.html", counter =sumSession())
+    print session['counter']
+    return render_template("index.html", counter =sumSession(1))
+@app.route ('/result', methods = ['POST'])
+def res():
+    print 'counter'
+    return render_template("result.html", counter = sumSession(2))
 app.run(debug=True) 
+  
