6f5b7eb8cbc43cc5d073469ffab1268be8762ac3
Mounika <mounika.tunga9@gmail.com>
2017-06-09 16:04:44 -0700

    added reset button

--- Counter/counter.py
+++ Counter/counter.py
@@ -5,15 +5,21 @@ def sumSession(i):
     try:
         session['counter'] += i 
     except KeyError:
-        session['counter'] = 1
+        session['counter'] = 0
     return session['counter']
 @app.route('/')
 def index():
-    print session['counter']
     return render_template("index.html", counter =sumSession(1))
 @app.route ('/result', methods = ['POST'])
 def res():
-    print 'counter'
-    return render_template("result.html", counter = sumSession(2))
+    return render_template("index.html", counter = sumSession(2))
+@app.route('/clear', methods = ['POST'])
+def clearing():
+    try:
+        session['counter'] = 0
+    except ValueError:
+        session['counter'] = 0
+    return render_template("index.html", counter = session['counter'])
 app.run(debug=True) 
+
   
