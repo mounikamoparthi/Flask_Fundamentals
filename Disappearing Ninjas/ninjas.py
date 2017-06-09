816c59691e9dd00d93433b8afa2e32ec96c64c80
Mounika <mounika.tunga9@gmail.com>
2017-06-08 22:43:30 -0700

    linked ninjacolor to the images

--- Disappearing Ninjas/ninjas.py
+++ Disappearing Ninjas/ninjas.py
@@ -6,4 +6,8 @@ def index():
 @app.route('/ninja', methods = ['POST'])
 def ninjas():
     return render_template("ninja.html")  
+@app.route('/ninja/<ninja_color>')
+def ninjacolor(ninja_color):
+    print ninja_color
+    return render_template("ninja.html",ninja_color = ninja_color )
 app.run(debug=True) 
