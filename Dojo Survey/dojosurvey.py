ca8f5ef506d1a2fa9389d8cfc609f0e100d8b1c3
Mounika <mounika.tunga9@gmail.com>
2017-06-10 22:10:13 -0700

    added flash messages

--- Dojo Survey/dojosurvey.py
+++ Dojo Survey/dojosurvey.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask,flash,render_template, request, redirect,session
 app = Flask(__name__)   #instantiation
+app.secret_key = 'keepitsecret'
 @app.route('/')
 def index():
     return render_template("index.html") 
 @app.route('/process', methods=['POST'])
 def Dojosurvey():
+    if len(request.form['name']) < 1:
+        flash("Name cannot be empty!")
+    else:
+        flash("Success! Your name is {}".format(request.form['name']))
+    if len(request.form['language']) < 1:
+        flash("language cannot be empty!")
+    else:
+        flash("Success! Your favlanguage is {}".format(request.form['language']))
+    return redirect('/')
     print request.form
     name = request.form['name']
     location = request.form['location']
