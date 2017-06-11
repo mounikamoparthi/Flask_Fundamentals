0ac224ff91e7e9d897004ad7ff18c3d8aa963003
Mounika <mounika.tunga9@gmail.com>
2017-06-10 18:19:51 -0700

    added flash messages

--- Great number game/game.py
+++ Great number game/game.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask,flash, render_template, request, redirect, session
+import random
 app = Flask(__name__)
+app.secret_key = 'abhbbibi'
+def setSession():
+    session['computer_input']=random.randint(1,100) 
 @app.route('/')
 def index():
-    print "Hello I'am ready"
+    try:
+        session['computer_input'] 
+    except KeyError:
+        setSession()
+
+    print session['computer_input']
     return render_template('index.html')
+
 @app.route('/result', methods = ['POST'])
-def randomnum():
-    return render_template('index.html')
+def game():
+    num = request.form ['number']
+    right = None
+    wrong = None
+    if int(num) == session['computer_input']:
+        flash( "Play again", 'right')
+        return redirect('/')
+    elif int(num) > session['computer_input']:
+        flash( "Too High!", 'toohigh')
+    elif int(num) < session['computer_input']:
+        flash ("Too Low!", 'toolow')
+    else:
+        flash('error', 'wrong')
+    return redirect('/')
+@app.route('/reset', methods=['POST'])
+def reset():
+    setSession()
+    return redirect('/')
+
 app.run(debug = True)
